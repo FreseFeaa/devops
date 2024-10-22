e394be500bb2aa8e79711da3e4585d7db2a7c7dd
Палехов Фёдор <f.palekhov@domain.local>
2024-10-22 13:59:10 +0300

    lefthook setup

--- apps/express-server/app.js
+++ apps/express-server/app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/person', (req, res) => {
-    console.log(req.body)
-    res.sendStatus(201)
-})
+app.post("/api/person", (req, res) => {
+	console.log(req.body);
+	res.sendStatus(201);
+});
 
-app.listen(3000)
+app.listen(3000);
